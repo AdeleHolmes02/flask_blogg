b312ca783f50ba5e6db9c239847e0ec29c98968a
AdeleHolmes02 <adelecholmes@gmail.com>
2026-06-03 23:34:12 +0200

    Implement create post form

--- app.py
+++ app.py
@@ -15,17 +15,22 @@ def show_post(post_id):
     post = database.get_post(post_id)
     return render_template("post.html", post=post)
 
+
 @app.route("/posts/new", methods=["GET", "POST"])
 def new_post():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
 
+        if not title or not body:
+            return render_template("new_post.html")
+
         database.create_post(title, body)
 
         return redirect(url_for("home"))
 
     return render_template("new_post.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
